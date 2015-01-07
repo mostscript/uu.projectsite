63f27a41f8197056ee32935185da8d465195507c
Sean Upton <sdupton@gmail.com>
2015-01-07 00:51:12 -0700

    patch ZServer access logging to omit OPTIONS requests from haproxy (ported from upiqsite.projects)

--- uu/projectsite/zope2.py
+++ uu/projectsite/zope2.py
@@ -3,4 +3,23 @@ import config
 
 def initialize(context):
     config.register()  # register plugin roles for collective.teamwork
+    patch_access_logging()
+
+
+def patch_access_logging():
+    """
+    Monkey patch access logging with a wrapper that returns
+    before emitting any messages for OPTIONS requests from haproxy
+    health checks.
+    """
+    from ZServer.AccessLogger import AccessLogger
+    _orig = AccessLogger.log
+
+    def newlog(self, message):
+        if 'OPTIONS / HTTP/1.0' in message:
+            return  # do not emit log message for haproxy health check
+        _orig(self, message)
+
+    setattr(AccessLogger, 'log', newlog)
+
     
