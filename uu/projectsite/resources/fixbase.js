007297451a9310cbddc3c217cbbc8ed6c3e2269c
Sean Upton <sdupton@gmail.com>
2016-08-13 21:18:52 -0600

    base tag rewrite js preserves any existing trailing slash

--- uu/projectsite/resources/fixbase.js
+++ uu/projectsite/resources/fixbase.js
@@ -7,9 +7,13 @@
       existing = head.getElementsByTagName('base'),
       base = document.createElement('base'),
       loc = window.location,
+      slash = existing.length && existing.attr('href').slice(-1) === '/',
       href = loc.href.replace(loc.search, '').replace(loc.hash, '');
   if (existing.length) {
     head.removeChild(existing[0]);
+    if (slash) {
+      href += '/';
+    }
   }
   base.setAttribute('href', href);
   if (head.firstChild) {
